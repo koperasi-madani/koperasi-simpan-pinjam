a60dbdcc53934d28c624cec7d7d179c2bb299fa6
Rifjan Jundila <rjndla@gmail.com>
2023-06-05 17:54:51 +0700

    Update SetorTunaiController.php

--- app/Http/Controllers/SetorTunaiController.php
+++ app/Http/Controllers/SetorTunaiController.php
@@ -247,7 +247,7 @@ class SetorTunaiController extends Controller
         $file_path = public_path('pdf/setor/') .$filename;
         $pdf->save($file_path);
 
-        return response()->json(['file_path' =>public_path('pdf/setor/') .$filename]);
+        return response()->json(['file_path' => asset('pdf/setor/'.$filename)]);
 
     }
 }
