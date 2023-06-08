053066c2dd608b8cc0b233d832f982b5e5cdbd3f
Rifjan Jundila <rjndla@gmail.com>
2023-06-08 22:46:16 +0700

    Update index.blade.php

--- resources/views/pages/setor-tunai/index.blade.php
+++ resources/views/pages/setor-tunai/index.blade.php
@@ -107,16 +107,15 @@
                 method: 'POST',
                 data: {transaction: response.transaction},
                 success: function(response) {
-                    console.log(response);
                     var receiptUrl = response.file_path;
-                    // var link = document.createElement('a');
-                    // link.href = receiptUrl;
-                    // link.download = 'receipt.pdf';
-                    // link.target = '_blank'; // Untuk membuka tautan unduhan dalam tab baru
-                    // document.body.appendChild(link);
-                    // link.click();
-                    // document.body.removeChild(link);
-                    // setTimeout(redirectToPage(`{{ route('teller.informasi.nasabah') }}`), 50000);
+                    var link = document.createElement('a');
+                    link.href = receiptUrl;
+                    link.download = 'receipt.pdf';
+                    link.target = '_blank'; // Untuk membuka tautan unduhan dalam tab baru
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+                    setTimeout(redirectToPage(`{{ route('teller.informasi.nasabah') }}`), 50000);
                 },
                 error: function(xhr, status, error) {
                     // Tangani kesalahan jika terjadi
