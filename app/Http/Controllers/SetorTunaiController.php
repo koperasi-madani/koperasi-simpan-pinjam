3a327a6e05ee68016baea6849fd9a0922b040d88
Rifjan Jundila <rjndla@gmail.com>
2023-06-08 21:59:02 +0700

    Update SetorTunaiController.php

--- app/Http/Controllers/SetorTunaiController.php
+++ app/Http/Controllers/SetorTunaiController.php
@@ -276,10 +276,8 @@ class SetorTunaiController extends Controller
         $pdf = PDF::loadHTML($html);
         $pdf->setPaper('A4', 'portrait');
         $filename = $transaction['kode'].'.'.'pdf';
-        $file_path = public_path('laravel/public/pdf/setor/') .$filename;
+        $file_path = public_path('pdf/setor/') .$filename;
         $pdf->save($file_path);
-        return asset('laravel/public/pdf/setor/'.$filename);
-        // return response()->json(['file_path' => asset('pdf/setor/'.$filename)]);
-
+        return response()->json(['file_path' => asset('laravel/public/pdf/setor/'.$filename)])
     }
 }
