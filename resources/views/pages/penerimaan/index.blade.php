1747316cdba5a6b7d23bd751760342980de2c2b9
Rifjan Jundila <rjndla@gmail.com>
2023-09-02 11:15:43 +0700

    fix:kas pagi

--- resources/views/pages/penerimaan/index.blade.php
+++ resources/views/pages/penerimaan/index.blade.php
@@ -229,8 +229,8 @@
                         <span class="icon icon-sm rounded-circle bg-primary-light"><i class="text-primary material-icons md-monetization_on"></i></span>
                         <div class="text">
                             <h6 class="mb-1 card-title">Saldo Teller</h6>
-                            <input type="number" name="pembayaran" id="pembayaran" value="{{ isset($pembayaran) ? $pembayaran - $denominasi : 0 }}" hidden>
-                            <span>Rp. {{ number_format(isset($pembayaran) ? $pembayaran - $denominasi : 0 ,2, ",", ".") }}</span>
+                            <input type="number" name="pembayaran" id="pembayaran" value="{{ isset($pembayaran) ? $pembayaran - $nominal_denominasi : 0 }}" hidden>
+                            <span>Rp. {{ number_format(isset($pembayaran) ? $pembayaran - $nominal_denominasi : 0 ,2, ",", ".") }}</span>
                         </div>
                     </article>
                     <hr>
